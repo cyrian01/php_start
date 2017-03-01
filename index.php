0acb9b7d1b99410d483b77da931dea71481e2c1d
robot206 <cyrian206@gmail.com>
2017-03-01 13:57:21 +0300

    modification du fichier index

---
+++ index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>PHP Test</title>
+    <link rel="stylesheet" href="./assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="./assets/css/font-awesome.min.css">
+    <link rel="stylesheet" href="./assets/css/style.css">
+
+</head>
+<body>
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-3 case1 text-center">
+                    <i class="fa fa-home fa-4x">  Home</i>
+                </div>
+                <div class="col-md-3 case2">
+                    <i class="fa fa-video-camera fa-4x"> Video</i>
+                </div>
+                <div class="col-md-3 case3">
+                    <i class="fa fa-music fa-4x"> Music</i>
+                </div>
+                <div class="col-md-3 case4">
+                    <i class="fa fa-envelope fa-4x"> Message</i>
+                </div>
+
+            </div>
+                <div class="row">
+                    <div class="col-sm-3 log">
+                         <form>
+                            <div class="form-group">
+                                <input type="text" name="pseudo" placeholder="Pseudo" id="psd" class="form-control"/>
+                                <label for="psd">Votre pseudo</label>
+                                <input type="password" name="password" placeholder="Mot de passe" id="pass" class="form-control"/>
+                                <label for="pssword">Votre mot de passe</label>
+                                <input type="submit" value="connexion" class="form-control"/>
+                            </div> 
+                        </form>
+                    </div>
+                </div>
+
+        </div>
+<script src="./assets/js/jquery.min.js"></script>
+<script src="./assets/js/bootstrap.min.js"></script>
+</body>
+
+</html>
