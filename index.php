28767b0471064c4524395044357dee7633701544
robot206 <cyrian206@gmail.com>
2017-03-03 10:40:06 +0300

    Modification general

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>PHP Test</title>
+    <title>ESSAI VIDEO</title>
     <link rel="stylesheet" href="./assets/css/bootstrap.min.css">
     <link rel="stylesheet" href="./assets/css/font-awesome.min.css">
     <link rel="stylesheet" href="./assets/css/style.css">
@@ -18,13 +18,26 @@
                     <i class="fa fa-video-camera fa-4x"> Video</i>
                 </div>
                 <div class="col-md-3 case3">
-                    <i class="fa fa-music fa-4x"> Sound</i>
+                    <i class="fa fa-music fa-4x"> Music</i>
                 </div>
                 <div class="col-md-3 case4">
-                    <i class="fa fa-envelope fa-4x"> Contact</i>
+                    <i class="fa fa-envelope fa-4x"> Message</i>
                 </div>
 
             </div>
+                <div class="row">
+                    <div class="col-sm-3 log">
+                         <form>
+                            <div class="form-group">
+                                <input type="text" name="pseudo" placeholder="Pseudo" id="psd" class="form-control"/>
+                                <label for="psd">Votre pseudo</label>
+                                <input type="password" name="password" placeholder="Mot de passe" id="pass" class="form-control"/>
+                                <label for="pssword">Votre mot de passe</label>
+                                <input type="submit" value="connexion" class="form-control"/>
+                            </div> 
+                        </form>
+                    </div>
+                </div>
 <script src="./assets/js/jquery.min.js"></script>
 <script src="./assets/js/bootstrap.min.js"></script>
 </body>
