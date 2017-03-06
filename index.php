199d31711121bc16a2fd2047ea7072864315c89d
robot206 <cyrian206@gmail.com>
2017-03-06 13:15:04 +0300

    Modification avec nav

--- index.php
+++ index.php
@@ -2,29 +2,33 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>ESSAI VIDEO</title>
+    <title>Video.Net</title>
     <link rel="stylesheet" href="./assets/css/bootstrap.min.css">
     <link rel="stylesheet" href="./assets/css/font-awesome.min.css">
     <link rel="stylesheet" href="./assets/css/style.css">
 
 </head>
 <body>
-        <div class="container-fluid">
-            <div class="row">
-                <div class="col-md-3 case1 text-center">
-                    <i class="fa fa-home fa-4x">  Home</i>
-                </div>
-                <div class="col-md-3 case2">
-                    <i class="fa fa-video-camera fa-4x"> Video</i>
-                </div>
-                <div class="col-md-3 case3">
-                    <i class="fa fa-music fa-4x"> Music</i>
-                </div>
-                <div class="col-md-3 case4">
-                    <i class="fa fa-envelope fa-4x"> Message</i>
-                </div>
+		<div class="header">
+		<a href="index.php"><h1><img src="./assets/image/00.ico" class="logo"/>VIDEO.NET</h1></a>
+		</div>
+        <div class="navbar navbar-renverse">
+             <div class="container-fluid">
 
-            </div>
+                    <ul class="nav navbar-nav">
+                        <li class="navbarbtns"><a href="index.php">Accueil</a></li>
+                        <li class="navbarbtns"><a href="#">Nouveautés</a><li>
+                        <li class="navbarbtns"><a href="#">Les plus téléchargés</a></li>
+                    </ul>
+
+                    <ul class="nav navbar-nav navbar-right">
+                            <li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
+                            <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+                    </ul>
+
+             </div>
+        </div>
+        <div class="container-fluid"> 
                 <div class="row">
                     <div class="col-sm-3 log">
                          <form>
@@ -38,6 +42,7 @@
                         </form>
                     </div>
                 </div>
+        </div>
 <script src="./assets/js/jquery.min.js"></script>
 <script src="./assets/js/bootstrap.min.js"></script>
 </body>
