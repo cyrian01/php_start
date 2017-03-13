e660a382f7a541aa2405fd368fd49e73e5b18e6f
robot206 <cyrian206@gmail.com>
2017-03-13 14:44:35 +0300

    Exercice table de mult

--- index.php
+++ index.php
@@ -22,17 +22,24 @@ function test($p1,$p2){
         return $p1." ".$p2;
     }
 }
+function mult($m){
+    $res="";
+    for($i=1; $i<=10; $i++){
+        $res=$res.' '.$m.' * '.$i.' = '.$m*$i.'<br>';
+    }
+    return $res;
+}
 
     $v1=4;
     $v2=7;
     $b='<br>';
-    compar($v1,$v2);
+ /*   compar($v1,$v2);
     echo $b;
-    echo $b;
+    echo $b; */
     $v3=$v1;
     $v1=$v2;
     $v2=$v3;
-    echo $v1;
+   /* echo $v1;
     echo $b;
     echo $b;
     echo $v2;
@@ -47,5 +54,78 @@ function test($p1,$p2){
     echo $b;
     soustract($v1,$v2);
     echo $b;
-    test(4,4);
+    echo $b;
+    echo test(4,4);
+    echo $b;
+    echo $b;
+    echo mult(2); */
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Exercice</title>
+    <link rel="stylesheet" href="./assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="./assets/css/font-awesome.min.css">
+    <link rel="stylesheet" href="./assets/css/style.css">
+
+</head>
+<body>
+        <div class="container-fluid">
+                <div class="row">
+                    <div class="col-md-3 table1 text-center">
+<?php 
+    echo "Table 2";
+    echo $b;
+    echo $b;
+    echo mult(2);
+    echo $b;
+    echo $b;
+    echo "Table 6";
+    echo $b;
+    echo mult(6);
+?>
+                    </div>
+                    <div class="col-md-3 table2 text-center">
+<?php 
+    echo "Table 3";
+    echo $b;
+    echo $b;
+    echo mult(3);
+    echo $b;
+    echo $b;
+    echo "Table 7";
+    echo $b;
+    echo mult(7);
+?>
+                    </div>
+                    <div class="col-md-3 table3 text-center">
+<?php 
+    echo "Table 4";
+    echo $b;
+    echo $b;
+    echo mult(4);
+    echo $b;
+    echo $b;
+    echo "Table 8";
+    echo $b;
+    echo mult(8);
+?>
+                    </div>
+                    <div class="col-md-3 table4 text-center">
+<?php 
+    echo "Table 5";
+    echo $b;
+    echo $b;
+    echo mult(5);
+    echo $b;
+    echo $b;
+    echo "Table 9";
+    echo $b;
+    echo mult(9);
+?>
+                    </div>
+                </div>
+        </div>
+
+</body>
