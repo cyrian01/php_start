50e4d9ee96770d5c645a606e96139b72f57c3e44
robot206 <cyrian206@gmail.com>
2017-03-13 15:43:43 +0300

    integration du php avec bootstrap

--- index.php
+++ index.php
@@ -73,58 +73,14 @@ function mult($m){
 <body>
         <div class="container-fluid">
                 <div class="row">
-                    <div class="col-md-3 table1 text-center">
-<?php 
-    echo "Table 2";
-    echo $b;
-    echo $b;
-    echo mult(2);
-    echo $b;
-    echo $b;
-    echo "Table 6";
-    echo $b;
-    echo mult(6);
-?>
-                    </div>
-                    <div class="col-md-3 table2 text-center">
-<?php 
-    echo "Table 3";
-    echo $b;
-    echo $b;
-    echo mult(3);
-    echo $b;
-    echo $b;
-    echo "Table 7";
-    echo $b;
-    echo mult(7);
-?>
-                    </div>
-                    <div class="col-md-3 table3 text-center">
-<?php 
-    echo "Table 4";
-    echo $b;
-    echo $b;
-    echo mult(4);
-    echo $b;
-    echo $b;
-    echo "Table 8";
-    echo $b;
-    echo mult(8);
-?>
-                    </div>
-                    <div class="col-md-3 table4 text-center">
-<?php 
-    echo "Table 5";
-    echo $b;
-    echo $b;
-    echo mult(5);
-    echo $b;
-    echo $b;
-    echo "Table 9";
-    echo $b;
-    echo mult(9);
-?>
-                    </div>
+                <?php 
+                    for($i=2; $i<=20; $i++){
+                        echo '<div class="col-md-2 text-center"><br>';
+                        echo 'Table '.$i.'<br>';
+                        echo mult($i);
+                        echo '</div>'; 
+                    }
+                ?>
                 </div>
         </div>
 
